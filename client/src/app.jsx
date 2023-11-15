cc1315638e5e86ccd4fe4421ad2c5ea2f12b6f2c
malloryporche <malloryburke@gmail.com>
2023-11-15 14:12:37 -0800

    Reverted app.jsx code

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,15 +2,9 @@ import React from 'react';
 import FiveStars from './sharedComponents/FiveStars.jsx';
 
 const App = () => {
-  const props = 2.5;
 
   return (
-    <div>
       <h1>Hello World!!!!!</h1>
-
-      <FiveStars props={props}/>
-
-    </div>
   )
 }
 
