4e34c7892f1ca93d2fac23e5ab23d180a3a67a2c
KenMain5 <kennyize15@gmail.com>
2023-11-16 15:10:18 -0500

    included sharedcomps - button

---
+++ client/src/sharedComponents/customButton/CustomButton.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './customButton.scss'
+
+//To control the width of the button, create a div around the Button JSX and set the specific width of that button to
+//the desired width of the button. Accepts 'text', 'Icon', 'onClickFunction', 'buttonWidth' and 'args' properties. 
+//Example: Minimum needede to render <Button text='open' Icon={StarBorderIcon}buttonWidth="460px" />
+function Button({text, Icon, buttonWidth, onClickFunction, args }) {
+
+  const handleOnClick = () => {
+    if (onClickFunction) {
+      onClickFunction(args)
+    }
+  }
+
+  return (
+      <button type='button' onClick={() => handleOnClick()} className='sharedButton' style={{width: buttonWidth}}>
+        <div className='sharedButton__container' style={{justifyContent: Icon ? 'space-between': 'center' }}>
+          <div className='sharedButton__text'>
+              <span>{text}</span>
+          </div>
+          {Icon && <div className='sharedButton__image'>
+            <Icon/>
+          </div>}
+        </div>
+      </button>
+  )
+}
+
+export default Button
