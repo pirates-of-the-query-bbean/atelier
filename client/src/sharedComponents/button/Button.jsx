f03b76802d648c7bbd9a34b16853c4e1353279e7
KenMain5 <kennyize15@gmail.com>
2023-11-15 19:11:15 -0500

    turned div into a button for better user accessability

--- client/src/sharedComponents/button/Button.jsx
+++ client/src/sharedComponents/button/Button.jsx
@@ -3,6 +3,7 @@ import './Button.css'
 
 //To control the width of the button, create a div around the Button JSX and set the specific width of that button to
 //the desired width of the button. Accepts 'text', 'Icon', 'onClickFunction', 'buttonWidth' and 'args' properties. 
+//Example: Minimum needede to render <Button text='open' Icon={StarBorderIcon}buttonWidth="460px" />
 function Button({text, Icon, buttonWidth, onClickFunction, args }) {
 
   const handleOnClick = () => {
@@ -12,7 +13,7 @@ function Button({text, Icon, buttonWidth, onClickFunction, args }) {
   }
 
   return (
-      <div onClick={() => handleOnClick()} className='sharedButton' style={{width: buttonWidth}}>
+      <button type='button' onClick={() => handleOnClick()} className='sharedButton' style={{width: buttonWidth}}>
         <div className='sharedButton__container' style={{justifyContent: Icon ? 'space-between': 'center' }}>
           <div className='sharedButton__text'>
               <span>{text}</span>
@@ -21,7 +22,7 @@ function Button({text, Icon, buttonWidth, onClickFunction, args }) {
             <Icon/>
           </div>}
         </div>
-      </div>
+      </button>
   )
 }
 
