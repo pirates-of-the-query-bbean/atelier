a423a3d69a35b5f5ad40ccfa82cb06db391a3e16
sawyer1776 <blackcreekproductions@gmail.com>
2023-11-15 21:59:18 -0500

    esLint installed and configured runs on npm run start-webpack and should also run on change in a file.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import styles from './App.module.scss'
+import styles from './App.module.scss';
 
-
-const App = () => {
-
+function App() {
   return (
     <div>
-
       <h2 className={styles.ugly}>Pirates of the query-bbean</h2>
     </div>
-  )
+  );
 }
 
 export default App;
