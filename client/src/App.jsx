cd957be86ea6897d759c3ec6e46b0ab58b2090bd
malloryporche <malloryburke@gmail.com>
2023-11-16 11:48:48 -0800

    Installed FiveStars node modules

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styles from './App.module.scss';
+import FiveStars from './sharedComponents/FiveStars.jsx';
 
 function App() {
+
   return (
     <div>
       <h1 data-testid="app-hw" className={styles.ugly}>Pirates of the query-bbean</h1>
