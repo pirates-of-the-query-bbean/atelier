f1606b7de2c870919fd5d4a739dd0fb3d33a2439
malloryporche <malloryburke@gmail.com>
2023-11-17 15:53:13 -0800

    Removed closure for checking userVotes

--- client/src/sharedComponents/upvoteLink/UpvoteLink.jsx
+++ client/src/sharedComponents/upvoteLink/UpvoteLink.jsx
@@ -2,24 +2,14 @@ import React, {useState} from 'react';
 import styles from './Upvote.module.scss';
 
 // Because various components have different keys, pass in key along with object to upvote a particular property.
-// <Helpful item={questionObj/answerObj/reviewObj} property={helpfulnessKey}/>
+//EXAMPLE USAGE: <UpvoteLink item={questionObj/answerObj/reviewObj} property={helpfulnessKey}/>
 
 const UpvoteLink = ({item, property}) => {
 
   const [upvoteCount, setUpvoteCount] = useState(item[property]);
 
   const upvote = (item, property) => {
-    var userVoted = false;
-
-    return (item, property) => {
-      if (!userVoted) {
-        setUpvoteCount(item[property]+= 1);
-        userVoted = true;
-      } else {
-        console.log('You can only upvote this item once.')
-      }
-    }
-
+    setUpvoteCount(item[property]+= 1);
   }
 
   return (
