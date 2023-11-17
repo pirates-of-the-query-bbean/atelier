2f698dcdbf82f5cf6438476f8cfcdcff0dd75dde
malloryporche <malloryburke@gmail.com>
2023-11-16 16:58:21 -0800

    Helper function for converting answer object

--- client/src/questions/Question.jsx
+++ client/src/questions/Question.jsx
@@ -1,18 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import AnswerList from './AnswerList.jsx';
 
 const Question = ({ product }) => {
   const [questionArr, setQuestionArr] = useState([]);
-  const [AnswerArrayArr, setAnswerArrayArr] = useState([]);
+  const [answerArr, setAnswerArr] = useState([]);
+
+  const getAnswers = (answerObj) => {
+    const answerArr = [];
+
+    for (let key in answerObj) {
+      answerArr.push(answerObj[key]);
+    }
+  }
 
   const testAnswer = {
-    "id": 70,
-    "body": "Some of the seams started splitting the first time I wore it!",
-    "date": "2019-11-28T00:00:00.000Z",
-    "answerer_name": "sillyguy",
-    "helpfulness": 6,
-    "photos": [],
+    73: {
+      "id": 73,
+      "body": "Some of the seams started splitting the first time I wore it!",
+      "date": "2019-11-28T00:00:00.000Z",
+      "answerer_name": "sillyguy",
+      "helpfulness": 6,
+      "photos": [],
+    },
+    78: {
+      "id": 78,
+      "body": "9 lives",
+      "date": "2019-11-12T00:00:00.000Z",
+      "answerer_name": "iluvdogz",
+      "helpfulness": 31,
+      "photos": [],
+    },
+    70: {
+      "id": 70,
+      "body": "Some of the seams started splitting the first time I wore it!",
+      "date": "2019-11-28T00:00:00.000Z",
+      "answerer_name": "sillyguy",
+      "helpfulness": 6,
+      "photos": [],
+    }
   }
+
+
+  useEffect(() => {
+    setAnswerArr(getAnswers(testAnswer));
+  }, [])
 
   return (
     <div className='question__container'>
