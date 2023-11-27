77f83f2dd0c92558798c3ff37a3f0c81bae38e07
Colin Cooley <colincooley1989@gmail.com>
2023-11-27 09:24:20 -0900

    fixed some Linter issues

--- client/src/widgets/RatingsReviews/RatingBreakdown.jsx
+++ client/src/widgets/RatingsReviews/RatingBreakdown.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import styles from './RatingBreakdown.module.scss'; // Make sure to create a corresponding SCSS file
+import styles from './RatingBreakdown.module.scss';
 
 const RatingBreakdown = function ({ productReviews, reviewAverage }) {
   let recommendPercentage = 0;
-  let ratingCounts = { 5: 0, 4: 0, 3: 0, 2: 0, 1: 0 };
+  const ratingCounts = {
+    5: 0,
+    4: 0,
+    3: 0,
+    2: 0,
+    1: 0,
+  };
 
   if (productReviews && productReviews.results) {
-    // Calculate the recommendation percentage
     recommendPercentage = (
-      (productReviews.results.filter(review => review.recommend).length / productReviews.results.length) * 100
+      (productReviews.results.filter(review => review.recommend).length
+        / productReviews.results.length) * 100
     ).toFixed(0);
 
-    // Count the number of reviews per star
     productReviews.results.forEach(review => {
       if (ratingCounts.hasOwnProperty(review.rating)) {
         ratingCounts[review.rating]++;
@@ -19,25 +24,32 @@ const RatingBreakdown = function ({ productReviews, reviewAverage }) {
     });
   }
 
-  // Calculate the width percentage of each rating bar
   const totalReviews = productReviews.results ? productReviews.results.length : 0;
   let ratingPercentages = {};
   for (const [key, value] of Object.entries(ratingCounts)) {
     ratingPercentages[key] = totalReviews > 0 ? ((value / totalReviews) * 100).toFixed(0) : 0;
   }
 
-
   return (
     <div className={styles.ratingBreakdown}>
-      <div className={styles.averageRating}>{reviewAverage} <span className={styles.stars}>★★★★★</span></div>
+      <div className={styles.averageRating}>
+        {reviewAverage}
+        <span className={styles.stars}>
+          ★★★★★
+        </span>
+      </div>
       <div className={styles.recommendation}>
-        {recommendPercentage}% of reviews recommend this product
+        {recommendPercentage}
+        % of reviews recommend this product
       </div>
       {Object.keys(ratingCounts).reverse().map(star => (
         <div key={star} className={styles.ratingRow}>
           <div className={styles.starLabel}>{star} stars</div>
           <div className={styles.ratingBarContainer}>
-            <div className={styles.ratingBar} style={{ width: `${ratingPercentages[star]}%` }}></div>
+            <div className={styles.ratingBar}
+              style={{ width: `${ratingPercentages[star]}%` }}>
+              {/* bar */}
+            </div>
           </div>
         </div>
       ))}
