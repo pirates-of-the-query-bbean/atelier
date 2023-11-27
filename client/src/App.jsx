f801bfa4f0b08b32553aa4b166005b71b9d1e442
malloryporche <malloryburke@gmail.com>
2023-11-27 07:27:37 -0800

    Added API call to upvote

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import styles from './App.module.scss';
 import UpvoteLink from './sharedComponents/upvoteLink/UpvoteLink.jsx';
-import RatingsReviews from './widgets/RatingsReviews/RatingsReviews.jsx';
-import FiveStars from './sharedComponents/FiveStars';
+// import RatingsReviews from './widgets/RatingsReviews/RatingsReviews.jsx';
 
 function App() {
 
@@ -11,15 +10,17 @@ function App() {
   const [currentProduct, setCurrentProduct] = useState({});
   const [productReviews, setProductReviews] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
     getProducts();
+    getQuestions();
   }, []);
 
   const getProducts = () => {
     axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products', {
       headers: {
-        'Authorization': process.env.REACT_APP_API_KEY
+        'Authorization': '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'
       }
     })
     .then((response) => {
@@ -36,7 +37,7 @@ function App() {
     setIsLoading(true);
     axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?product_id=${currentProduct.id}`, {
       headers: {
-        'Authorization': process.env.REACT_APP_API_KEY
+        'Authorization': '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'
       }
     })
     .then((response) => {
@@ -49,6 +50,21 @@ function App() {
       setIsLoading(false);
     })
   }
+
+  const getQuestions = () => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/questions', {
+      headers: {
+        Authorization: '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',
+      },
+    })
+      .then((response) => {
+        setQuestions(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log('error fetching questions', err);
+      });
+  };
 
   useEffect(() => {
     if (currentProduct && currentProduct.id) {
@@ -66,7 +82,8 @@ function App() {
       <h1 data-testid="app-hw" className={styles.ugly}>
         Pirates of the query-bbean
       </h1>
-      <RatingsReviews productReviews={productReviews} />
+      {/* <RatingsReviews productReviews={productReviews} /> */}
+      {/* <UpvoteLink item={questions[0]} itemType="question" property="question_helpfulness" /> */}
     </div>
   );
 }
--- client/src/sharedComponents/upvoteLink/UpvoteLink.jsx
+++ client/src/sharedComponents/upvoteLink/UpvoteLink.jsx
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import styles from './Upvote.module.scss';
 
 // Because various components have different keys, pass in key
 // along with object to upvote a particular property.
 // EXAMPLE USAGE: <UpvoteLink item={questionObj/answerObj/reviewObj} property={helpfulnessKey}/>
 
-function UpvoteLink({ item, property }) {
+function UpvoteLink({ item, itemType, property }) {
   const [upvoteCount, setUpvoteCount] = useState(item[property]);
+  let upvoteURL = '';
+
+  if (itemType === 'review') {
+    upvoteURL = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/${item.id}/helpful`;
+  } else if (itemType === 'answer') {
+    upvoteURL = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/answers/${item.id}/helpful`;
+  } else {
+    upvoteURL = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/questions/${item.id}/helpful`;
+  }
 
   const upvote = (obj, key) => {
+    axios({
+      method: 'put',
+      url: upvoteURL,
+      headers: {
+        Authorization: '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',
+      },
+    });
+
     const upvoteObj = obj;
     setUpvoteCount(upvoteObj[key] += 1);
   };
