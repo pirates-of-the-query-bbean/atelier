c958ef683b07f3520e28f99123cbef22dcf5dfa1
sawyer1776 <blackcreekproductions@gmail.com>
2023-11-29 22:09:10 -0500

    adding prop validation

--- client/src/widgets/Overview/Description.jsx
+++ client/src/widgets/Overview/Description.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import CheckIcon from '@mui/icons-material/Check';
 import styles from './Description.module.scss';
 import SocialShare from './SocialShare';
@@ -29,5 +30,25 @@ function Description({ product }) {
     </div>
   );
 }
+
+Description.defaultProps = {
+  product: {
+    slogan: '',
+    description: '',
+    features: null,
+  },
+};
+
+Description.propTypes = {
+  product: PropTypes.shape({
+    slogan: PropTypes.string,
+    description: PropTypes.string,
+    features: PropTypes.arrayOf({
+      value: PropTypes.string,
+      feature: PropTypes.string,
+    }),
+  }),
+
+};
 
 export default Description;
